d1184c6660edcaaa5b3d064605be67bf0752ef8f
DalekiyM4rs <evgeniybulychev@bk.ru>
2024-07-11 19:01:31 +0300

    DB cfg

---
+++ src/main/resources/DB/initDB.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS users;
+CREATE SEQUENCE global_seq START WITH 100000;
+
+CREATE TABLE users
+(
+    id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    chat_id    INTEGER UNIQUE                NOT NULL,
+    name       VARCHAR                       NOT NULL,
+    phone_num  VARCHAR                       NOT NULL,
+    bot_scpecs VARCHAR                       NOT NULL,
+    bot_state  VARCHAR                       NOT NULL
+);
+
